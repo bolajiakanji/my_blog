11246af71cd5c9638e4738023eb5b32536975cf7
lukman bolaji <bolajiakanji21@gmail.com>
2024-10-20 03:34:54 -0700

    Refactor word animation

---
+++ app/hooks/useAnimatingWords.ts
@@ -0,0 +1,54 @@
+import { useRef, useState, useEffect } from "react";
+
+const useAnimatingWords = (wordArrays: string[]) => {
+  const wordCount = useRef(0);
+  const [word, setWord] = useState<string>(wordArrays[wordCount.current]);
+  const [wordletters, setWordLetters] = useState<string[]>([]);
+  const [letterCount, setCount] = useState<number>(1);
+
+  useEffect(() => {
+    let reset: NodeJS.Timeout;
+    const interval = setInterval(() => {
+      if (letterCount > word.split("").length) {
+        clearInterval(interval);
+        wordCount.current++;
+
+        if (wordCount.current >= wordArrays.length) wordCount.current = 0;
+        wordCount.current = wordCount.current;
+        console.log(wordCount.current);
+        reset = setTimeout(() => {
+          setWord(wordArrays[wordCount.current]);
+          setWordLetters([]);
+          setCount(1);
+        }, 2000);
+        return;
+      }
+
+      if (reset) clearTimeout(reset);
+
+      word.split("").forEach((letter, index) => {
+        if (index + 1 !== letterCount) return;
+
+        setWordLetters((eachLetter) => {
+          let newarray = [...eachLetter];
+          newarray.push(letter);
+          return newarray;
+        });
+      });
+
+      setCount(letterCount + 1);
+    }, 50);
+
+    return () => {
+      clearInterval(interval);
+      clearTimeout(reset);
+    };
+  }, [letterCount, word]);
+  return {
+    word,
+    wordletters,
+    letterCount,
+  };
+};
+
+export default useAnimatingWords;
--- app/page.tsx
+++ app/page.tsx
@@ -1,68 +1,12 @@
 'use client'
-import Image from "next/image";
-import { useEffect, useRef, useState } from "react";
-import { clearInterval, setInterval } from "timers";
 
+import useAnimatingWords from "./hooks/useAnimatingWords";
 
-  
-const array: string[] = ['1bkmfgfdvdgvgvdcdgdvg', '2jdhdgfgd df d ddgddg', '3uygfdd  fdsdfsdsfssf']
-
+const skills: string[] = ['1bkmfgfdvdgvgvdcdgdvg', '2jdhdgfgd df d ddgddg', '3uygfdd  fdsdfsdsfssf']
 
 export default function Home() {
-
-  const pro = useRef(0)
+  const { wordletters } = useAnimatingWords(skills)
   
-  const [me, setme] = useState<string>(array[pro.current])
-  const [you, setyou] = useState < string[]>([]) 
-  const [yo, setyo] = useState < string[]>([]) 
-  const [cot, setcot] = useState <number>(1) 
- 
-  useEffect(()=>{
-    let reset: NodeJS.Timeout
-    const rem = setInterval(() => {
-      if (cot > me.split('').length) {
-        clearInterval(rem)
-        pro.current++
-        if (pro.current >= array.length) pro.current=0
-        pro.current = pro.current
-        console.log(pro.current)
-       reset = setTimeout(() => {
-          setme(array[pro.current])
-          setyou([])
-          setcot(1)
-          
-        }, 2000);
-    return 
-    
-    
-      }
-    if (reset)  clearTimeout(reset)
-
-   me.split('').forEach((a, b, c) => {
-      if ((b + 1) !== cot) return
-      
-      setyou((meyou) => {
-        let newarray = [...meyou]
-        newarray.push(a)
-          
-        return newarray
-      })
-        
-    
-         
-
-        
-      })
-      
-    setcot(cot + 1)
-
-  
-}, 50)
-    return () => {
-      clearInterval(rem);
-      clearTimeout(reset)
-    }
-  }, [cot, me]) ;
   
     return (
     <>
@@ -70,7 +14,7 @@ export default function Home() {
         <div className="relative  w-40 h-32 bg-cyan-900 px-3">
           
           <div className="left-0 text-center  absolute w-full bg-yellow-400">nna</div></div>
-        {you.map((s) => <span>{s}</span>)}
+        {wordletters.map((s) => <span>{s}</span>)}
         
   </>
   
