61eb72df34a988121d274a1e2f932c0fc51d0a3d
lukman bolaji <bolajiakanji21@gmail.com>
2024-12-03 22:00:27 -0800

    create client bounding context

---
+++ app/context/clientBounding.ts
@@ -0,0 +1,13 @@
+"use client"
+
+import React, { SetStateAction } from "react"
+
+interface Wrap {
+    currentBoundingClient: string;
+    setCurrentBoundingClient: React.Dispatch<SetStateAction<string>>
+}
+
+
+const ClientBounding = React.createContext<Wrap>({} as Wrap)
+
+export default ClientBounding
