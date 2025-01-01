e25357bbc0398af3647b6caa0c39de3d2961b1aa
lukman bolaji <bolajiakanji21@gmail.com>
2025-01-01 05:14:49 -0800

    change overlay light background color to white

--- app/components/project/Overlaywrapper.tsx
+++ app/components/project/Overlaywrapper.tsx
@@ -5,7 +5,7 @@ const Overlaywrapper = ({ children }: PropsWithChildren) => {
   return (
     <Box className="fixed w-full left-0 top-0 dark:bg-bg_color bg-white bg-opacity-80 dark:bg-opacity-80 backdrop-blur-md z-40 h-full overflow-hidden">
           <Box
-        className="mx-auto  bg-bg_color w-full md:w-80% overflow-x-hidden h-screen"
+        className="mx-auto  bg-white dark:bg-bg_color w-full md:w-80% overflow-x-hidden h-screen"
           >
               {children}
           </Box>
