6ebd49b711e1e87497fc01a54ea193dd125ba702
lukman bolaji <bolajiakanji21@gmail.com>
2025-01-01 12:12:53 -0800

    Add hide-all button

--- app/components/Cerificatition.tsx
+++ app/components/Cerificatition.tsx
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 import CertificatitionWrapper from "./CertificationWrapper";
 import { Box, Button, Card, Flex, Text } from "@radix-ui/themes";
 
-const certs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const certs = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const progressive_num = 3;
 
 const Certificatition = () => {
   const [cert, setCert] = useState(certs.slice(0, progressive_num));
   const [showLess, setShowLess] = useState(false);
 
- // const reminder = certs.length % cert.length;
+  // const reminder = certs.length % cert.length;
   //const quotent = Math.floor(certs.length / cert.length);
 
   const new_cert = cert.slice().length;
@@ -26,22 +26,31 @@ const Certificatition = () => {
     <CertificatitionWrapper>
       {/* <Box style={{height: '1000px'}} className='h-screen'>Cerificatition</Box> */}
       <Flex gap="7" justify="center" className="flex-wrap mb-3 mt-7">
-      {cert.map((cer) => (
-        <Card
-      className="sm:max-w-96 h-40  md:max-w-80 md:mx-0 lg:max-w-96 w-full ">
-          {cer}
-        </Card>
-      ))}
-        </Flex>
+        {cert.map((cer) => (
+          <Card className="sm:max-w-96 h-40  md:max-w-80 md:mx-0 lg:max-w-96 w-full ">
+            {cer}
+          </Card>
+        ))}
+      </Flex>
       <Box className="flex justify-center ">
         {certs.length > 4 && cert.length < certs.length && (
           <Button
             onClick={handleclick}
-            className="rounded-s-md"
+            className={`rounded-s-md ${cert.length > 3 ? '' : 'rounded-e-md'} `}
             variant="classic"
             radius="none"
           >
             See more
+          </Button>
+        )}
+        {cert.length > 6 && (
+          <Button
+            radius="none"
+            color="amber"
+            className="rounded-s-md"
+            onClick={() => setCert(certs.slice(0, 3))}
+          >
+            Hide all
           </Button>
         )}
         {showLess && cert.length > progressive_num && (
@@ -56,7 +65,6 @@ const Certificatition = () => {
           </Button>
         )}
       </Box>
-        <Text as="span" size="1" className="relative -top-7 ms-3 md:ms-16" onClick={() => setCert(certs.slice(0, 3))}>Hide all</Text>
     </CertificatitionWrapper>
   );
 };
